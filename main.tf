32b0dc75cc0ba5851072e86fa10c2a0e04f1a194
Vikram <vikram.purohits@.com>
2024-09-11 00:14:16 +0530

    my-terraform-script

---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "aws"  {
+	region = "us-west-1"
+	
+	}
+
+
+#variable "cidr_block" {}
+#variable "environment" {}
+
+resource "aws_vpc" "dev-vpc" {
+
+	cidr_block = var.vpc_cidr_block[0]
+	tags = {
+	Name = "dev"
+	}
+}
+
+
+resource "aws_vpc" "prod-vpc" {
+
+        cidr_block = var.vpc_cidr_block[1]
+        tags = {
+        Name = "dev"
+        }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+	vpc_id = aws_vpc.dev-vpc.id
+	cidr_block = var.subnet_cidr_block[0]
+	tags = {
+	Name = "dev"
+	}
+}
+
+
+
+
+resource "aws_subnet" "dev-subnet-2" {
+        vpc_id = aws_vpc.dev-vpc.id
+        cidr_block = var.subnet_cidr_block[1]
+        tags = {
+        Name = "dev"
+        }
+}
+
+resource "aws_subnet" "dev-subnet-3" {
+        vpc_id = aws_vpc.dev-vpc.id
+        cidr_block = var.subnet_cidr_block[2]
+        tags = {
+        Name = "dev"
+        }
+}
+
+output "vpcIdOutput" {
+
+value = aws_vpc.dev-vpc.id
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "subnet_cidr_block" {
+description = "cidr-block for subnet"	
+type = list(string)
+default = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+}
+
+variable "vpc_cidr_block" {
+description = "cidr-block for vpc"
+type = list(string)
+default = ["10.1.0.0/16", "192.168.0.0/16"]
+}
+
+#variable "environment" {}
+#subnet_cidr_block = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+#vpc_cidr_block = ["10.1.0.0/16", "192.168.0.0/16"]
