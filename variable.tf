350376af4ae16353309079a6e703faf402a3ac15
shanuvik <vikramp.2104@gmail.com>
2024-09-11 00:31:10 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -9,7 +9,7 @@ description = "cidr-block for vpc"
 type = list(string)
 default = ["10.1.0.0/16", "192.168.0.0/16"]
 }
-
+#
 #variable "environment" {}
 #subnet_cidr_block = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
 #vpc_cidr_block = ["10.1.0.0/16", "192.168.0.0/16"]
